8c6e9e01a1e8c97f43cdbc547df1ac696c355f9c
Cryptofowl <86430321+Cryptofowl@users.noreply.github.com>
2021-11-17 19:14:01 -0500

    Create braidOrbit.js
    Initializes OrbitDB instance, defines functions for managing data

---
+++ dev/braidOrbit.js
@@ -0,0 +1,99 @@
+class braidOrbit {
+    constructor (Ipfs, OrbitDB) {
+        this.Ipfs = Ipfs
+        this.OrbitDB = OrbitDB
+    }
+
+    async create() {
+        this.node = await this.Ipfs.create({
+            preload: {enabled: false },
+            repo: './ipfs',
+            EXPERIMENTAL: { pubsub: true},
+            config: {
+                Bootstrap: [],
+                Addresses: { Swarm: [] }
+            }
+        })
+
+        this._init()
+    }
+
+    async _init () {
+        this.orbitdb = await this.OrbitDB.createInstance(this.node)
+        this.defaultOptions = { accessController: {
+            write: [this.orbitdb.identity.id]
+            }
+        }
+        const docStoreOptions = {
+            ...this.defaultOptions,
+            indexBy: 'hash',
+        }
+        this.papers = await this.orbitdb.docstore('papers', docStoreOptions)
+        await this.papers.load()
+
+        this.onready()
+    }
+ 
+    async addNewPaper(hash, author, title, date, doi) {
+        const existingPaper = this.getPaperByHash(hash)
+        if (existingPaper) {
+            await this.updatePaperByHash(hash, author, title, date, doi)
+            return
+        }
+
+        const cid = await this.papers.put({ hash, author, title, date, doi})
+        return cid
+    }
+
+    getAllPapers() {
+        const papers = this.papers.get('')
+        return papers
+    }
+
+    getPaperByHash(hash) {
+        const singlePaper = this.papers.get(hash)[0]
+        return singlePaper
+    }
+
+    getPaperByAuthor(author) {
+        return this.pieces.query((paper) => paper.author === author)
+    }
+
+    getPaperByTitle(title) {
+        return this.pieces.query((paper) => paper.title === title)
+    }
+
+    getPaperByDate(date) {
+        return this.pieces.query((paper) => paper.date === date)
+    }
+
+    getPaperByDoi(doi) {
+        return this.pieces.query((paper) => paper.doi === doi)
+    }
+
+    async updatePaperByHash (hash, author, title, date, doi) {
+        const paper = await.this.getPaperByHash(hash)
+        paper.author = author
+        paper.title = title
+        paper.date = date
+        paper.doi = doi
+        const cid = await this.paper.put(paper)
+        return cid
+    }
+
+    async deletePaperByHash (hash) {
+        const cid = await this.paper.del(hash)
+        return cid
+    }
+
+}
+
+try {
+    const Ipfs = require('ipfs')
+    const OrbitDB = require('orbit-db')
+
+    module.exports = exports = new braidOrbit(Ipfs, OrbitDB)
+} catch (e) {
+    window.NPP = new braidOrbit(window.Ipfs, window.OrbitDB)
+}
+
