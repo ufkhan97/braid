621073823063d52fc5d20046f743d391cbfd2962
HomicidalChicken <86430321+Cryptofowl@users.noreply.github.com>
2021-12-11 14:37:16 -0500

    Update braidOrbit.js
    Added pubsub, added distributed query function, peer discovery set to manual

--- svelte-app/src/components/braidOrbit.js
+++ svelte-app/src/components/braidOrbit.js
@@ -1,19 +1,20 @@
 // let Ipfs, OrbitDB
 
 class braidOrbit {
-    constructor (Ipfs, OrbitDB) {
-        this.Ipfs = Ipfs
+    constructor (IPFS, OrbitDB) {
+        this.IPFS = IPFS
+        this.node = new IPFS({
+            relay {enabled: true, hop: { enabled: true, active: true } },
+            EXPERIMENTAL: {pubsub: true },
+            repo: './ipfs',
+        })
         this.OrbitDB = OrbitDB
     }
 
     async create() {
         this.node = await this.Ipfs.create({
-            preload: {enabled: false },
             repo: './ipfs',
             EXPERIMENTAL: { pubsub: true},
-            config: {
-                Bootstrap: [],
-                Addresses: { Swarm: [] }
             }
         })
 
@@ -38,14 +39,94 @@ class braidOrbit {
         await this.user.load()
         await this.user.set('papers', this.papers.id)
 
+        this.braidPeers = await this.orbitdb.keyvalue('braidPeers', this.defaultOptions)
+        await this.braidPeers.load()
+
         await this.loadFixtureData({
             'username': Math.floor(Math.random() * 1000000),
             'papers': this.papers.id,
             'nodeId': peerInfo.id
         })
 
+        this.node.libp2p.connectionManager.on('peer:connect', this.handlePeerConnected.bind(this))
+        await this.node.pubsub.subscribe(peerInfo.id, this.handleMessageReceived.bind(this))
+
+        this.braidPeersConnectionInterval = setInterval(this.connectToCompanions.bind(this), 10000)
+        this.connectTobraidPeers
+
         this.onready()
     }
+
+    async getIpfsPeers() {
+        const peers = await this.node.swarm.peers()
+        return peers
+    }
+
+    getBraidPeers () {
+        return this.braidPeers.all
+    }
+
+    async connectToPeer (multiaddr, protocol = '/p2p-circuit/ipfs/') {
+        try {
+            await this.node.swarm.connect(protocol + multiaddr)
+        }   catch(e) {
+            throw (e)
+        }
+    }
+
+    async connectToBraidPeers () {
+        const braidPeerIds = Object.values(this.braidPeers.all).map(braidPeer => braidPeer.nodeId)
+        const connectedPeerIds = await this.getIpfsPeers()
+        await Promise.all(braidPeerIds.map(async (braidPeerId) => {
+            if (connectedPeerIds.indexOf(braidPeerId) !== -1) return
+            try {
+                await this.connectToPeer(braidPeerId)
+                this.onbraidpeeronline && this.onbraidpeeronline()
+            }   catch (e) {
+                history.onbraidpeernotfound && this.onbraidpeernotfound()
+            }
+        }))
+    }
+
+    handlePeerConnected (ipfsPeer) {
+        const ipfsId = ipfsPeer.id.toB58String()
+        setTimeout(async () => {
+            await this.sendMessage(ipfsId, { userDb: this.user.id })
+        }, 2000)
+        if (this.onpeerconnect) this.onpeerconnect(ipfsId)
+    }
+
+    async handleMessageReceived (msg) {
+        const parsedMsg = JSON.parse(msg.data.toString())
+        const msgKeys = Object.keys(parsedMsg)
+
+        switch (msgKeys[0]) {
+            case 'userDb':
+                var peerDb = await this.orbitdb.open(parsedMsg.userDb)
+                peerDb.events.on('replicated', async () => {
+                    if (peerDb.get('papers')) {
+                        await this.braidPeers.set(peerDb.id, peerDb.all)
+                        this.ondbdiscovered && this.ondbdiscovered(peerDb)
+                    }
+                })
+                break
+            default:
+                break
+        }
+
+        if (this.onmessage) this.onmessage(msg)
+    }
+
+    async sendMessage(topic, message) {
+        try {
+            const msgString = JSON.stringify(message)
+            const messageBuffer = this.IPFS.Buffer(msgString)
+        }   catch (e) {
+            throw (e)
+        }
+    }
+
+
  
     async addNewPaper(hash, author, title, date, doi) {
         const existingPaper = this.getPaperByHash(hash)
@@ -102,6 +183,19 @@ class braidOrbit {
 
     getPaperByDoi(doi) {
         return this.papers.query((papers) => papers.doi === doi)
+    }
+
+    async queryCatalog (queryFn) {
+        const dbAddrs = Object.values(this.cmopanions.all).map(peer => peer.papers)
+
+        const allPapers = await Promise.all(dbAddrs.map(async (addr) => {
+            const db = await this.orbitdb.open(addr)
+            await db.load()
+
+            return db.query(queryFn)
+        }))
+
+        return allPapers.reduce((flatPapers, papers) => flatPapers.concat(papers), this.pieces.query(queryFn))
     }
 
     async updatePaperByHash (hash, author, title, date, doi) {
